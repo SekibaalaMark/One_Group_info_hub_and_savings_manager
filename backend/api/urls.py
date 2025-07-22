3164df9a2807d17466315377df8ff11520990fa4
sekibaalamark <markmajje256@gmail.com>
2025-07-22 19:40:31 +0300

    created urls for my App

---
+++ backend/api/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('verify-email/', VerifyEmailCodeView.as_view(), name='verify-email'),
+]
