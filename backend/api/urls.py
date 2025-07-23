ac93e379e3b1abe48425125f0efb9b165fe73dfc
sekibaalamark <markmajje256@gmail.com>
2025-07-23 19:19:56 +0300

    created urls for refrencing the login API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('verify-email/', VerifyEmailCodeView.as_view(), name='verify-email'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
 ]
