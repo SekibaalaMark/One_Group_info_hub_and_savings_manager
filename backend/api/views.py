0c7087fe76409194a3b03aa1601870c69860914a
sekibaalamark <markmajje256@gmail.com>
2025-07-23 19:42:27 +0300

    created a view to confirm password reset

--- backend/api/views.py
+++ backend/api/views.py
@@ -128,3 +128,13 @@ class PasswordResetRequestView(APIView):
 
             return Response({"message": "Password reset link sent."}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class PasswordResetConfirmView(APIView):
+    def post(self, request):
+        serializer = PasswordResetConfirmSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Password reset successful."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
