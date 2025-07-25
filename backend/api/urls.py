2a7849e8fa05cfd715c7ec29e0cfad5f96815125
sekibaalamark <markmajje256@gmail.com>
2025-07-25 17:49:52 +0300

    added a savings api url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('password-reset-request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('save/', SavingCreateView.as_view(), name='save-money'),
 ]
