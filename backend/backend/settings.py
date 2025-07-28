43df5be724cab0b3258e8abc78be668dfcf50d98
sekibaalamark <markmajje256@gmail.com>
2025-07-28 16:56:07 +0300

    set up the settings for deployment and neon db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,13 +69,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        "postgresql://neondb_owner:npg_VUR4Ch3XkiFa@ep-hidden-moon-afj0pqwf-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
